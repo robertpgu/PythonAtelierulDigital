b693b2f798daf51223459950261bfceabcc6566b
robertpgu <contact.robert00@gmail.com>
2021-11-04 19:32:35 +0200

    Instructiuni

---
+++ Curs2.py
@@ -0,0 +1,49 @@
+# if condition:
+#     print('e adevarat')
+# elif condition:
+#     print('se respecta alta conditie')
+# else:
+#     print('alt caz')
+#
+# my_var = 5
+# msg = 'Nicio conditie nu este respectata'
+# msg1 = 0
+# if my_var < 6:
+#     msg, msg1 = "Set instructiuni 1", my_var
+# elif my_var > 10:
+#     msg = "Set instructiuni 2"
+# print(msg, msg1)
+#
+# a = 1;
+# b = 2;
+# x = 1 if a > b else -1
+#  var = sintaxa_then if conditie else sintaxa_else
+# print(x)
+#
+# while True:
+#     print("Set instructiuni")
+#     break
+#
+# for i in range(10):
+#     print(f'Set instructiuni {i}')
+#
+# variabila = 'Ana are mere'
+# lista = []
+# for i in variabila:
+#     lista.append(i)
+# lista = [for i in variabila]
+# print(lista)
+#
+# for item,value in enumerate(variabila):
+#     print(f"{item} => {value}")
+
+# dictionar = {"key1": "value1" , "key2": "value2"}
+# for item in dictionar:
+#     # print(item)
+#     item0, item1 = item
+#     print(item0, item1)
+
+# variabila = (1,)
+# print(type(variabila))
+variabila = "Ana \"are\" mere"
+print(variabila)
